13211c544878511c06defcd72bc1608f8ceb077d
Ahmadreza Bashari <ahmadrezabashari@gmail.com>
2025-12-26 16:12:26 +0330

    Initialize and fork each child app from its entry point

--- config.php
+++ config.php
@@ -5,12 +5,11 @@ return [
     'apps' => [
         'paths' => __DIR__."/apps/",
 
-        /**
-        'manual_paths' => [
-           'http' => __DIR__."/apps/http/",
-           'mqtt' => __DIR__."/apps/mqtt/",
-           'socket' => __DIR__."/apps/socket/",
-        ],
-         */
+        /** Manual Paths */
+        //  'manual_paths' => [
+        //  'http' => __DIR__."/apps/http/",
+        //  'mqtt' => __DIR__."/apps/mqtt/",
+        //  'socket' => __DIR__."/apps/socket/",
+        //  ],
     ],
 ];
